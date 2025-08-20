ddfbba06fb070e2e4128de006d7a76a4fcb9c209
Volkan Genç <gencvolkan@gmail.com>
2025-08-21 00:31:15 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    //.SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddUserSecrets<Program>();
+   //last one win...
+
+    IConfiguration configuration = builder.Build();
+
+    Console.WriteLine("Reading Logging values...");
+    var appSettings = configuration.GetSection("Logging");
+     Console.WriteLine($"AppSettings: {appSettings["LogLevel:Default"]}");
+
+     Console.WriteLine(configuration.GetValue<int>("StarterCountValue"));
+
+Console.WriteLine(configuration.GetValue<string>("User:Name"));
+
+Console.WriteLine("my script: " + configuration.GetValue<string>("MySecretKey"));
+
+Console.ReadLine();
